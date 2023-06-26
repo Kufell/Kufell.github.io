1b6e0814901e7bc41b4222e25698afd7f1ca0823
Kufell <qflvty@gmail.com>
2023-06-26 11:22:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.getElementById('toggle-en').addEventListener('click', function() {
     this.classList.add('active');
     document.getElementById('toggle-pl').classList.remove('active');
-    document.getElementById('text-en').classList.remove('hidden');
-    document.getElementById('text-pl').classList.add('hidden');
   });
   
   document.getElementById('toggle-pl').addEventListener('click', function() {
     this.classList.add('active');
     document.getElementById('toggle-en').classList.remove('active');
-    document.getElementById('text-pl').classList.remove('hidden');
-    document.getElementById('text-en').classList.add('hidden');
   });
