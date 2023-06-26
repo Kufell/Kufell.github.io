0277659342d3acfe15a3b6d29b33e0df20fabd1e
Kufell <qflvty@gmail.com>
2023-06-26 12:11:23 +0200

    a

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 function toggleLanguage(lang) {
     var toggleEn = document.getElementById('toggle-en');
     var togglePl = document.getElementById('toggle-pl');
-    var textEn = document.getElementById('text-en');
-    var textPl = document.getElementById('text-pl');
+    var textSections = document.getElementsByClassName('text-section');
   
     if (lang === 'en') {
       toggleEn.classList.add('active');
       togglePl.classList.remove('active');
-      if (textEn) {
-        textEn.style.display = 'block';
-      }
-      if (textPl) {
-        textPl.style.display = 'none';
+  
+      for (var i = 0; i < textSections.length; i++) {
+        var textSection = textSections[i];
+        if (textSection.classList.contains('text-en')) {
+          textSection.style.display = 'block';
+        } else {
+          textSection.style.display = 'none';
+        }
       }
     } else if (lang === 'pl') {
       togglePl.classList.add('active');
       toggleEn.classList.remove('active');
-      if (textPl) {
-        textPl.style.display = 'block';
-      }
-      if (textEn) {
-        textEn.style.display = 'none';
+  
+      for (var i = 0; i < textSections.length; i++) {
+        var textSection = textSections[i];
+        if (textSection.classList.contains('text-pl')) {
+          textSection.style.display = 'block';
+        } else {
+          textSection.style.display = 'none';
+        }
       }
     }
   }
   
   // Execute toggleLanguage function on page load to set the initial language
   toggleLanguage('en');
+  
+  
+  
+  
